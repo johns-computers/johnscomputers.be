5177490f9782f1bd3215838aaa1425729401f984
IchBinHanz <nilssimons707@gmail.com>
2022-04-21 17:57:43 +0200

    1.0
    First Commit

---
+++ public/assets/js/utils/includes.js
@@ -0,0 +1,25 @@
+function includeHTML() {
+    var incAtr = 'include'
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute(incAtr);
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    elmnt.removeAttribute(incAtr);
+                    includeHTML();
+                }
+            }      
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+};
+
+includeHTML()

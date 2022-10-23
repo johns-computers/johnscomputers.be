b37953c9a136fa4081f12478e7a988bead11738f
IchBinHanz <nilssimons707@gmail.com>
2022-10-23 21:38:10 +0200

    bb alance

--- public/assets/js/utils/lang.js
+++ public/assets/js/utils/lang.js
@@ -1,3 +1,18 @@
 var userLang = navigator.language || navigator.userLanguage; 
 
-alert(userLang)
+langs = {
+    "de-DE": "de",
+    "nl-NL": "nl",
+}
+
+
+if(langs[userLang]) {
+    console.log(langs[userLang]); 
+    fetch(`./assets/json/langs/${langs[userLang]}.json`)
+    .then(response => { return response.json() })
+    .then(data => {
+        data.elms.forEach(elm => {
+            document.querySelector(elm.querySelector).innerHTML = elm.text
+        })
+    });
+}
